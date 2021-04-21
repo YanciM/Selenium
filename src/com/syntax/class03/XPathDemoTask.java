bf39628a6b1aa893297a836b20401b94a911dd5a
YanciM <yancimontiel@gmail.com>
2021-04-20 22:05:33 -0400

    Second Commit

---
+++ src/com/syntax/class03/XPathDemoTask.java
@@ -0,0 +1,27 @@
+package com.syntax.class03;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XPathDemoTask {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Login.aspx?ReturnUrl=%2fsamples%2fTestComplete11%2fWebOrders%2fDefault.aspx");
+        driver.manage().window().maximize();
+        WebElement usernameTextBox=driver.findElement(By.xpath("//input[contains(@id,'username')]"));
+        usernameTextBox.sendKeys("Tester");
+        WebElement passwordTextBox= driver.findElement(By.xpath("//input[@type='password']"));
+        passwordTextBox.sendKeys("test");
+        WebElement loginButton= driver.findElement(By.xpath("//input[@value='Login']"));
+        loginButton.click();
+        Thread.sleep(2000);
+        WebElement logoutLink= driver.findElement(By.xpath("//a[text()='Logout']"));
+        logoutLink.click();
+        driver.quit();
+    }
+
+}
